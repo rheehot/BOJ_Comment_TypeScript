226762a5cfe62782036f9987f1f63fc2f3174839
riyenas0925 <riyenas0925@gmail.com>
2020-07-04 04:54:21 +0000

    ENH : 백준 홈페이지에서만 아이콘 활성화 기능 구현

--- src/eventPage.ts
+++ src/eventPage.ts
@@ -1,3 +1,36 @@
+const targetPage: string = "/https:\/\/www\.acmicpc\.net\/problem\/*/"
+
+chrome.tabs.onActivated.addListener(function (info) {
+    chrome.tabs.get(info.tabId, function (change) {
+        if(change.url == undefined){
+            chrome.browserAction.setPopup({tabId: info.tabId, popup: ''});
+            chrome.browserAction.setIcon({path: 'icon-disabled.png', tabId: info.tabId});
+        }
+        else if (change.url.match(targetPage) == null) {
+            chrome.browserAction.setPopup({ tabId: info.tabId, popup: '' });
+            chrome.browserAction.setIcon({ path: 'icon-disabled.png', tabId: info.tabId });
+        }
+        else {
+            chrome.browserAction.setPopup({ tabId: info.tabId, popup: 'popup.html' });
+            chrome.browserAction.setIcon({ path: 'icon.png', tabId: info.tabId });
+        }
+    });
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, change, tab) {
+    if(change.url == undefined){
+        return;
+    }
+    else if (tab.url.match(targetPage) == null) {
+        chrome.browserAction.setPopup({ tabId: tabId, popup: '' });
+        chrome.browserAction.setIcon({ path: 'icon-disabled.png', tabId: tabId });
+    }
+    else {
+        chrome.browserAction.setPopup({ tabId: tabId, popup: 'popup.html' });
+        chrome.browserAction.setIcon({ path: 'icon.png', tabId: tabId });
+    }
+});
+
 // Listen to messages sent from other parts of the extension.
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     // onMessage must return "true" if response is async.
